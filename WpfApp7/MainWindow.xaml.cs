32b62cb5e1e5d7f69a62ebde4068f3afd30e34bc
ivan.shatilov.06@mail.ru <ivan.shatilov.06@mail.ru>
2024-05-26 23:39:02 +0500

    Добавьте файлы проекта.

---
+++ WpfApp7/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApp7
+{
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer timer;
+        private int counter = 0;
+        private bool isCounting = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+        }
+
+        private void ToggleCounting(object sender, RoutedEventArgs e)
+        {
+            if (isCounting)
+            {
+                timer.Stop();
+            }
+            else
+            {
+                timer.Start();
+            }
+            isCounting = !isCounting;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            txtCounter.Text = counter.ToString();
+            counter++;
+        }
+    }
+}
